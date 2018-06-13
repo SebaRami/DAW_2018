7c012d0e66f2646db3382db492bdc6ef11b4cfa0
Estudiante <sebarami@espol.edu.ec>
2018-06-13 10:03:45 -0500

    privado

---
+++ Tareas/Ecmascript/js/app.js
@@ -0,0 +1,63 @@
+/*
+	Mi EcmaScript
+
+	consola web
+	2+2
+	'hola'
+	console.log('hola')
+	alert('Pop Up!')
+*/
+
+/*
+* Conversión de tipos
+*/
+
+/*console.log('Comentario '+100)
+console.log(52+ ' Comentario ')
+console.log("37" + 7)
+console.log("37" - 7)
+
+console.log(parseInt("37") + 7)
+console.log(parseInt("37") - 7)
+*/
+
+function saludar(nombre) {
+  return "Intro a " + nombre; //Cuando se llama, esta funcion devuelve "Hola " y el nombre que se le ha pasado como argumento.
+}
+
+/*Llamar a la función e imprimir por pantalla / mensaje de alerta*/
+
+/*
+* Funciones
+* var, let y const
+* BOM y DOM
+* Eventos
+*/
+
+(function() {
+	/*Mostrar un mensaje de alerta al cargar*/
+
+	/* Obtener el elemento con el id quote1 */
+
+	/* Obtener el elemento con el id quotes */
+
+	/* Obtener los elementos con la etiqueta div y mostrar el contenido*/
+
+	/* Obtener los elementos con la clase well */
+
+	/* Query Selector para los div's con clase well dentro del div con id quotes */
+
+	/* Mostrar los autores de las frases */
+
+	/* Mostrar el texto de los div's cuyo autor termine en ous */
+
+	/* Crear un h3 con el autor de cada div con clase well */
+
+	/* Insertar el h3 antes del p dentro de la cita */
+
+	/* Obtener el texto del input con id texto */
+
+	/* Al dar clic en el botón buscar debe ocultar las citas que no contengan el texto ingresado en input (id='texto')*/
+
+})();
+
