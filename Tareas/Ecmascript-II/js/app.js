f4bcacadb78fe4dc5950ec654fd830211a513ff9
Cliente CONTROL-PC <CltControl@GGIECSOFWRK131.espol.edu.ec>
2018-06-20 10:59:15 -0500

    Ecmascrip-II

--- Tareas/Ecmascript-II/js/app.js
+++ Tareas/Ecmascript-II/js/app.js
@@ -1,32 +1,44 @@
-(function(){
-	document.getElementById('buscar').onclick = function(){
-		text = document.getElementById('texto').value;
-		var divs = document.querySelectorAll('div#quotes > div');
-		for (div of divs){
-			var content = div.textContent;
-			if(content.includes(text)){
-				div.classList.add('mostrar');
-				div.classList.remove('ocultar');
-			}
-			else{
-				div.classList.add('ocultar');
-				div.classList.remove("mostrar");
-			}
+function onSuccessXml(data){
+	//var citas = data.getElementByTagName('cita');
+	//console.log(data);
+	$(data).find('cita').each(function(){
+      var author = $(this).find('autor').text();
+      var texto = $(this).find('texto').text();
+      $("<div></div>").attr('class','well').attr('author', author).append($("<p></p>").html(texto)).appendTo("#quotes");
+      //$("<div></div>").addClass('well').attr('author', author).html(texto).appendTo("#quotes");
+    });
+
+}
+
+function cargarCita(){
+	$.ajax({
+		type: "GET",
+		url: "data/citas.xml",
+		contentType: "text/xml",
+		success: onSuccessXml,
+	})
+}
+
+function busquedaFiltro(){
+	text = document.getElementById('texto').value;
+	var divs = document.querySelectorAll('div#quotes > div');
+	for (div of divs){
+		var content = div.textContent;
+		if(content.includes(text)){
+			div.classList.add('mostrar');
+			div.classList.remove('ocultar');
+		}
+		else{
+			div.classList.add('ocultar');
+			div.classList.remove("mostrar");
 		}
 	}
+}
 
-	function OnSuccess(data){
-		//var citas = data.getElementByTagName('cita');
-		console.log(data);
 
-	}
-
-	$.ajax({
-		type: 'GET',
-		url: 'data/citas.xml',
-		contentType: "text/xml",
-		dataType: "xml",
-		succes: OnSuccess,
-	})
-	
+(function(){
+	document.getElementById('buscar').onclick = busquedaFiltro;
+	document.getElementById('texto').onkeyup = busquedaFiltro;
+	$('#texto').on('keyup', busquedaFiltro);
+	cargarCita();
 })();
